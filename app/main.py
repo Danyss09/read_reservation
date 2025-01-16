3dff3673dfb42879f350701d20de6742105af165
Danyss09 <danielacaceresp4@gmail.com>
2025-01-16 16:58:10 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 # app/main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from app.database import get_db_connection
 
 app = FastAPI()
 
+# Ruta de prueba
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "Welcome to the Reservation Service!"}
+
+# Ruta para obtener todas las reservas
+@app.get("/read")
+async def read_reservations():
+    connection = await get_db_connection()
+    try:
+        query = "SELECT * FROM Reservations;"
+        reservations = await connection.fetch(query)
+        
+        # Si no hay reservas
+        if not reservations:
+            raise HTTPException(status_code=404, detail="No reservations found")
+        
+        # Convertir la respuesta en formato JSON
+        result = [dict(reservation) for reservation in reservations]
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {e}")
+    finally:
+        await connection.close()
