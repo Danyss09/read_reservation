e85114fb82fd6ca46191d1db03383282a4a623ac
Danyss09 <danielacaceresp4@gmail.com>
2025-01-29 17:30:26 -0500

    delete files

--- app/database.py
+++
@@ -1,6 +0,0 @@
-import asyncpg
-
-DATABASE_URL = "postgresql://postgres:dani0919@localhost:5432/ReservationDeleteDb"
-
-async def get_db_connection():
-    return await asyncpg.connect(DATABASE_URL)
